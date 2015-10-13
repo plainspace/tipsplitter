30f613044384bf78840fe37b6ac64799ae61ca61
Jared Volpe <plainspace@gmail.com>
2015-10-13 16:21:52 -0700

    Initial commit

---
+++ tips/README.md
@@ -0,0 +1,24 @@
+# CodePath iOS Bootcamp Prework - Tip Calculator
+
+This is Swift tip calculator application that is a requirement for the [CodePath iOS Bootcamp](https://codepath.com/iosbootcamp) appliation.
+
+Time spent: ~ 5 hours
+
+Completed user stories:
+
+* [x] Required: Create a tip calculator in Swift
+* [x] Optional: Change the colors and fonts of the views
+* [x] Optional: Add a custom font
+* [x] Optional: Add an app icon and launch image
+* [x] Optional: Add image assets
+* [ ] Optional: Add ability to split bill and view contribution per person
+* [ ] Optional: Hide split bill feature in a hidden subview and show after clicking button to split
+* [ ] Issue: Keep dollar sign visible after inputting bill amount
+* [x] Issue: If user clicks outside of input field and no amount is entered, show dollar sign
+* [ ] Issue: Fix bug where submitting bill value > deleting bill value > clicking outside of bill field > clicking back on bill field > entering new value > results in no calculation
+
+Check it out:
+
+![Video Walkthrough](anim_rotten_tomatoes.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -2,24 +2,89 @@
 //  ViewController.swift
 //  tips
 //
-//  Created by Jared on 10/6/15.
-//  Copyright © 2015 plainspace. All rights reserved.
+//  Created by Jared Volpe on 10/6/15.
+//  Copyright © 2015 plainspace.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var splitAmount: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var billAmount: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    // make the dollar sign stay after entering number
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        splitAmount.text = "$0.00"
+        splitLabel.text = "1"
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onAdd(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onSubtract(sender: AnyObject) {
+        
+    }
 
+    @IBAction func onEditingChanged
+        (sender: AnyObject) {
+            
+        let tipPercentages = [0.18, 0.2, 0.22]
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+            
+        if ( billField.text!.isEmpty )
+        {
+            billField.text = "$"
+        }
+            
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+            
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        let numberOfPeople = 2
+        let billPerPerson = total / 2
+        
+        splitAmount.text = "$\(billPerPerson)"
+        splitLabel.text = "\(numberOfPeople)"
+        
+        splitAmount.text = String(format: "$%.2f", billPerPerson)
+        splitLabel.text = String(format: "%u", numberOfPeople)
+            
+    }
+
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
 }
 
